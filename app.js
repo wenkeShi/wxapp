a54a67f4a25e64b16e47d54433f4ea0870967abe
shiwenke <shiwenke@fiberhome.com>
2017-10-23 17:01:30 +0800

    修改login中间件
    Signed-off-by: shiwenke <shiwenke@fiberhome.com>

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const login = require('./routes/login');
 // var http = require('http');
 
-//读取证书文件
+//获取认证证书
 var key = fs.readFileSync('./key/2_www.liudongtushuguan.cn.key');
 var cert = fs.readFileSync('./key/1_www.liudongtushuguan.cn_bundle.crt');
 
@@ -20,7 +20,7 @@ httpsServer.listen(443,() =>{
 	console.log('listening 443 port');
 });
 
-app.use('/login',login);
+app.use(login);
 app.get('/', (req , res) =>{
 	console.log('someone requested!');
 	res.cookie('mycookie','value',{time : new Date()});
--- routes/login.js
+++ routes/login.js
@@ -3,34 +3,38 @@ const router = express.Router();
 const https = require('https');
 const queryString = require('querystring');
 
-const APPID = 'wx3e1d175a787899bd ';
+const APPID = 'wx3e1d175a787899bd';
 const SECRET = '65c96753bb7b0bf6499c2df882b2c55a';
 const HOST = 'https://api.weixin.qq.com';
 const PATH = '/sns/jscode2session';
 
+
 router.route('/login')
 .get((req, res, next) => {
 	let code = req.query.code;
-
-	let option = {
-		host : HOST,
-		path : PATH,
-		method : 'GET',
-	};
-	let req = https.request(option, (res) => {
-		if(res.statusCode == 200){
-			console.log(res);
-			console.log(res.bady);
-		}
-	});
 	let data = {
 		appid : APPID,
 		secret : SECRET,
 		js_code : code,
 		grant_type : 'authorization_code',
 	};
-	req.write(querystring.stringify(data));
-	req.end();
+	let option = {
+		host : HOST,
+		path : PATH + '?'+queryString.stringify(data),
+		method : 'GET',
+	};
+	let wxReq = https.request(option, (res) => {
+		console.log('request wxopenid');
+		if(res.statusCode == 200){
+			console.log(res);
+			console.log(res.bady);
+		}
+	});
+
+	console.log(queryString.stringify(data));
+	//wxReq.write(queryString.stringify(data));
+	wxReq.end();
+	res.status(200).send('ok');
 	next();
 });
 
