66b33c5ca873f89be0ef25198184d00f9f1371fc
Shiwenke <swenke@foxmail.com>
2017-12-30 22:04:06 +0800

    modules break up

--- routes/router.js
+++ routes/router.js
@@ -21,28 +21,6 @@ const mongoose = DB.mongoose;
 const UserModel = Model.UserModel;
 const BookModel = Model.BookModel;
 
-
-
-const APPID = 'wx3e1d175a787899bd';
-const SECRET = '65c96753bb7b0bf6499c2df882b2c55a';
-const HOST = 'api.weixin.qq.com';
-const PATH = '/sns/jscode2session?';
-
-
-const DATA = {
-		appid : APPID,
-		secret : SECRET,
-		js_code : '',
-		grant_type : 'authorization_code',
-};
-const OPTION = {
-		host : HOST,
-		path : '',
-		method : 'GET',
-};
-// var   sessions = {};
-
-
 router.use(bodyParser.json());
 router.get('/register', register)
 .get('/login',login)
