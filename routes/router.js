bb78dd5bb22fb6900ae45e75b60d7b8d6e2d71e6
shiwenke <shiwenke@fiberhome.com>
2018-01-02 09:48:30 +0800

    modules break up

--- routes/router.js
+++ routes/router.js
@@ -28,6 +28,7 @@ const getBorrowMsgs = require('../service/getBorrowMsgs');
 const agree = require('../service/agree');
 const reject = require('../service/reject');
 const returnBook = require('../service/returnBook');
+const receiveBook = require('../service/receiveBook');
 
 const DB_CONNECTION = DB.connection;
 const mongoose = DB.mongoose;
@@ -81,55 +82,7 @@ router.get('/register', register)
 .get('/returnbook', returnBook)
 
 //收到书籍
-.post('/receivebook',(req, res, next) => {
-	let userId = sessions[req.headers.sessionid];
-	let body = req.body;
-	let bookId = body.bookId;
-	let borrowerId = body.borrowerId;
-	UserModel.findOne({openId : userId}, (err, owner) =>{
-		if(!err){
-			for(let i=0;i<owner.publishedBooks.length;i++){
-				if(owner.publishedBooks[i]._id == bookId){
-			  	owner.publishedBooks[i].borrower = '';
-			  	owner.publishedBooks[i].borrowerId = ''; //可以考虑加上borrowId
-			  	break;
-			  }
-			}
-			owner.markModified('publishedBooks');
-			owner.save();
-		}else{
-			console.log(err);
-		}
-	});
-//	UserModel.findOne({openId : borrowerId}, (err, borrower) => {
-//		if(!err){
-//			for(let i=0;i<borrower.borrowedBooks.length;i++){
-//				if(borrower.borrowedBooks[i].bookId == bookId){
-//			  	borrower.borrowedBooks[i].borrowingStatus = '已归还';
-//			  	break;
-//			  }
-//			}
-//			borrower.markModified('borrowedBooks');
-//			borrower.save();
-//			res.status(200).send();
-//			next();
-//		}else{
-//			console.log(err);
-//		}
-//	});
-	UserModel.update({openId : borrowerId, "borrowedBooks.bookId" : bookId}, {$set: {"borrowedBooks.$.borrowingStatus" : '已归还' } } ,(err, result) => {
-		if(!err){
-			res.status(200).send();
-			next();
-		}
-	});
-	BookModel.findOne({_id : bookId}, (err, book) => {
-		book.status = true;
-		book.save();
-		res.status(200).send();
-		next();
-	});
-})
+.post('/receivebook',receiveBook)
 
 
 module.exports = {
